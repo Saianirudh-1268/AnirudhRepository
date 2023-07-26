6e2e4704a48bc958b9b73587d3325f59bf1a07bb
G Sai Anirudh <saianirudh2000g@gmail.com>
2023-07-26 10:43:41 +0530

    Third commit

---
+++ Divison.java
@@ -0,0 +1,14 @@
+package com.qualizeal.javaapp1.operation;
+
+public class Divison{
+	public int divide(int num1,int num2){
+		System.out.println("divison started");
+		System.out.println("1st number = "+ num1);
+		System.out.println("2nd number = "+ num2);
+		
+		int result = 0;
+		int result = num1 / num2;
+		System.out.println("1st number/ 2nd number = "+ result);
+		System.out.println("divison ended");
+	}
+}
